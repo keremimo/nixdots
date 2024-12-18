11761eafa1a4cf017fcad83b0c0bb898448c08f5
Kerem Kilic <code@kerem.tech>
2024-12-18 23:25:29 +0100

    feat: Add gnome to T480

--- devices/t480/default.nix
+++ devices/t480/default.nix
@@ -10,6 +10,7 @@
   ];
 
   services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
   programs.niri.enable = true;
   programs.xwayland.enable = true;
 
